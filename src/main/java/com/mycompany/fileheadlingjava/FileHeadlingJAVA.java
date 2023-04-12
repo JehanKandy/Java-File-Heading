3496326ef5c15472514244a512babcc75321eb65
JehanKandy <jehankandy@gmail.com>
2023-04-12 08:53:15 +0530

    Update
    Co-Authored-By: Ishini Hathurusinghe <101358580+ishinihathurusinghe@users.noreply.github.com>

--- src/main/java/com/mycompany/fileheadlingjava/FileHeadlingJAVA.java
+++ src/main/java/com/mycompany/fileheadlingjava/FileHeadlingJAVA.java
@@ -18,5 +18,17 @@ public class FileHeadlingJAVA {
     public static void main(String[] args) {
         String inputFile = "input.txt";
         String outputFile = "output.txt";
+        
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+             FileWriter writer = new FileWriter(outputFile)) {
+             
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(line);
+                writer.write("\n"); // add a newline after each line
+            }
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 }
