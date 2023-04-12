b17a839f19baac455a85e11262b7f5245090fe66
JehanKandy <jehankandy@gmail.com>
2023-04-12 11:21:36 +0530

    Update
    Co-Authored-By: Ishini Hathurusinghe <101358580+ishinihathurusinghe@users.noreply.github.com>

--- src/main/java/com/mycompany/fileheadlingjava/FileHeadlingJAVA.java
+++ src/main/java/com/mycompany/fileheadlingjava/FileHeadlingJAVA.java
@@ -14,7 +14,18 @@ import java.util.Scanner;
 public class FileHeadlingJAVA {
 
     public static void main(String[] args) {
-        
-            
+        try {
+            File myObj = new File("readfile.txt");
+            myObj.getAbsolutePath();
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+              String data = myReader.nextLine();
+              System.out.println(data);
+            }
+            myReader.close();
+          } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+          }
+
     }
 }
