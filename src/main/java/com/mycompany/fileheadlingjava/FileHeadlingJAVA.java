7a444f1a2541f721adc0c4a5b7b1f0ad656f2abb
JehanKandy <jehankandy@gmail.com>
2023-04-12 08:39:15 +0530

    First Commit

---
+++ src/main/java/com/mycompany/fileheadlingjava/FileHeadlingJAVA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fileheadlingjava;
+
+/**
+ *
+ * @author Jehan
+ */
+public class FileHeadlingJAVA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
